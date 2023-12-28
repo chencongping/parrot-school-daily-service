10f72e9a64c101ba51b8734d9091788b868fd414
Chen Cong-Ping <1084337941@qq.com>
2023-12-29 02:20:26 +0800

    Initial project.

---
+++ src/main/java/com/parrot/chengcongping/parrot/school/daily/service/ParrotSchoolDailyServiceApplication.java
@@ -0,0 +1,13 @@
+package com.parrot.chengcongping.parrot.school.daily.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParrotSchoolDailyServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParrotSchoolDailyServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parrot/chengcongping/parrot/school/daily/service/ParrotSchoolDailyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parrot.chengcongping.parrot.school.daily.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParrotSchoolDailyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

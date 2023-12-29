8ad59c7794e01cb2744357a51d4a866fef7ab105
Chen Cong-Ping <1084337941@qq.com>
2023-12-30 03:49:38 +0800

    Add daily function.

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,3 +1,11 @@
+
+var today = new Date();
+var year = today.getFullYear();
+var month = today.getMonth() + 1;
+var day = today.getDate();
+var formattedDate = year + "-" + (month < 10 ? "0" + month : month) + "-" + (day < 10 ? "0" + day : day);
+$("#businessDate").val(formattedDate)
+
 function request(url, formData, success, error){
     $.ajax({
         type: 'POST',
